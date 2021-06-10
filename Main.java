9be417f37d4328e11227969f3add0a48f9e5550e
Asiyahsyh <asiyahsmisa@gmail.com>
2021-06-10 13:41:03 +0800

    project IDEA

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Asiyah";
+        int usia = 18;
+        int targetTahunKuliahh = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan);
+    }
+}
