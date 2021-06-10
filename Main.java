ff3d78b45902204b6f7799b89e87a4d2def7a24f
Asiyahsyh <asiyahsmisa@gmail.com>
2021-06-10 14:46:36 +0800

    Operasi Tipe String

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Asiyah";
@@ -12,5 +14,16 @@ public class Main {
         char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
         String uniskaStirng = new String(uniskaChar);
         System.out.println(uniskaStirng);
+
+        String namaLengkap = namaDepan;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("ASI"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(1,4));
+        System.out.println(namaLengkap.replace("A","H"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(2));
+
     }
 }
